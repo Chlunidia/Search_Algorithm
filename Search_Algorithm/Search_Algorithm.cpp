2495fb42ab556c1e6ecedfbb6d65f50c7e2ee484
Chlunidia Putri Kieswendha <kieswendha@gmail.com>
2023-11-25 11:23:47 +0700

    Implement input validation

--- Search_Algorithm/Search_Algorithm.cpp
+++ Search_Algorithm/Search_Algorithm.cpp
@@ -7,5 +7,22 @@ private:
     int i;
 
 public:
+    void input() {
+        while (true) {
+            std::cout << "Enter the number of elements in the array: ";
+            std::cin >> n;
+            if (n > 0 && n <= 20)
+                break;
+            else
+                std::cout << "\nArray should have minimum 1 and maximum 20 elements.\n";
+        }
 
+        std::cout << "\n----------------------";
+        std::cout << "\nEnter array elements ";
+        std::cout << "\n----------------------\n";
+        for (i = 0; i < n; i++) {
+            std::cout << "<" << (i + 1) << ">";
+            std::cin >> arr[i];
+        }
+    }
 };
