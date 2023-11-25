17c21ef5da50c4456bec8341b7108e5c2317eb64
Chlunidia Putri Kieswendha <kieswendha@gmail.com>
2023-11-25 11:24:25 +0700

    Implement binary search function

--- Search_Algorithm/Search_Algorithm.cpp
+++ Search_Algorithm/Search_Algorithm.cpp
@@ -25,4 +25,38 @@ public:
             std::cin >> arr[i];
         }
     }
+
+    void binarySearch() {
+        char ch;
+        do {
+            std::cout << "\nEnter element you want to search: ";
+            int item;
+            std::cin >> item;
+
+            int lowerbound = 0;
+            int upperbound = n - 1;
+            int mid = (lowerbound + upperbound) / 2;
+            int ctr = 0;
+
+            while (item != arr[mid] && lowerbound <= upperbound) {
+                if (item > arr[mid])
+                    lowerbound = mid + 1;
+                else
+                    upperbound = mid - 1;
+
+                mid = (lowerbound + upperbound) / 2;
+                ctr++;
+            }
+
+            if (item == arr[mid])
+                std::cout << "\n" << item << " found at position " << (mid + 1);
+            else
+                std::cout << "\n" << item << " not found in the array\n";
+
+            std::cout << "\nNumber of comparisons: " << ctr;
+            std::cout << "\nContinue search (y/n): ";
+            std::cin >> ch;
+
+        } while (ch == 'y' || ch == 'Y');
+    }
 };
